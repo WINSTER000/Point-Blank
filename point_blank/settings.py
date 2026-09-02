e16d20e16400b992beb8f32082cb2b989a80cc1f
Winster <146048623+WINSTER000@users.noreply.github.com>
2026-09-02 11:12:43 +0530

    Update settings.py

--- point_blank/settings.py
+++ point_blank/settings.py
@@ -64,11 +64,10 @@ WSGI_APPLICATION = 'point_blank.wsgi.application'
 # DATABASE
 DATABASES = {
     'default': dj_database_url.config(
-        env='DATABASE_URL',
-        conn_max_age=600,
-        ssl_require=True,
+        default=os.environ.get('DATABASE_URL')
     )
 }
+
 
 
 # EMAIL
